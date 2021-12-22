838ae43548546d7d3f72f59cf9e2ca42f7733bd9
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-23 00:50:34 +0530

    UPDATE: schema updates

--- api/schemas/post.js
+++ api/schemas/post.js
@@ -1,57 +1,57 @@
 export default {
-	name: "post",
-	title: "Post",
-	type: "document",
-	fields: [
-		{
-			name: "title",
-			title: "Title",
-			type: "string",
-		},
-		{
-			name: "description",
-			title: "Description",
-			type: "string",
-		},
-		{
-			name: "src",
-			title: "Source",
-			type: "url",
-		},
-		{
-			name: "category",
-			title: "Category",
-			type: "string",
-		},
-		{
-			name: "image",
-			title: "Image",
-			type: "image",
-			options: {
-				hotspot: true,
-			},
-		},
-		{
-			name: "userId",
-			title: "User ID",
-			type: "string",
-		},
-		{
-			name: "postedBy",
-			title: "Posted By",
-			type: "postedBy",
-		},
-		{
-			name: "save",
-			title: "Save",
-			type: "array",
-			of: [{ type: "save" }],
-		},
-		{
-			name: "comments",
-			title: "Comments",
-			type: "array",
-			of: [{ type: "comment" }],
-		},
-	],
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string"
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string"
+    },
+    {
+      name: "src",
+      title: "Source",
+      type: "url"
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string"
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: "userId",
+      title: "User ID",
+      type: "string"
+    },
+    {
+      name: "postedBy",
+      title: "Posted By",
+      type: "postedBy"
+    },
+    {
+      name: "save",
+      title: "Save",
+      type: "array",
+      of: [{ type: "save" }]
+    },
+    {
+      name: "comments",
+      title: "Comments",
+      type: "array",
+      of: [{ type: "comment" }]
+    }
+  ]
 };
--- api/schemas/user.js
+++ api/schemas/user.js
@@ -1,17 +1,17 @@
 export default {
-	name: "user",
-	title: "User",
-	type: "document",
-	fields: [
-		{
-			name: "userName",
-			title: "User Name",
-			type: "string",
-		},
-		{
-			name: "image",
-			title: "Image",
-			type: "image",
-		},
-	],
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string"
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string"
+    }
+  ]
 };
