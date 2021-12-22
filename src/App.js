f632c6e445f716bb1b4f70af301d09a2ac4cf4a8
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-22 19:56:01 +0530

    CREATE: routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
