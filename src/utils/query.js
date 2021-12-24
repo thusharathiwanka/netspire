967e8e25eb50fde2a07e6db01eabf1a5f342dc67
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-24 21:03:39 +0530

    CREAE: feed query created

--- src/utils/query.js
+++ src/utils/query.js
@@ -23,8 +23,21 @@ const searchQuery = (searchKeyword) => {
   return query;
 };
 
-const postsQuery = (userId) => {
-  const query = `*[_type == "user" && _id == "${userId}"]`;
+const postsQuery = () => {
+  const query = `*[_type == "post"] | order(_createdAt desc) {
+  image {
+      asset -> {
+        url
+      }
+    }, _id, destination, postedBy -> {
+      _id, userName, image
+    },
+    save[] {
+      _key, postedBy -> {
+        _id, userName, image
+      },
+    },
+  }`;
 
   return query;
 };
