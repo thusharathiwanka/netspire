3b45a2dba0e4b1283a41d9c28dca3d7e71f06764
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-24 20:58:25 +0530

    CREATE: search query created

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,5 +1,32 @@
-export const userQuery = (userId) => {
+const userQuery = (userId) => {
   const query = `*[_type == "user" && _id == "${userId}"]`;
 
   return query;
 };
+
+const searchQuery = (searchKeyword) => {
+  const query = `*[_type == "post" && title match == "${searchKeyword}*" || category match == "${searchKeyword}*" || about match == "${searchKeyword}*"] {
+    image {
+      asset -> {
+        url
+      }
+    }, _id, destination, postedBy -> {
+      _id, userName, image
+    },
+    save[] {
+      _key, postedBy -> {
+        _id, userName, image
+      },
+    },
+  }`;
+
+  return query;
+};
+
+const postsQuery = (userId) => {
+  const query = `*[_type == "user" && _id == "${userId}"]`;
+
+  return query;
+};
+
+export { userQuery, searchQuery, postsQuery };
