5d87d1e95c6719d87008fa9f2c6990b5de48b0fd
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-24 21:30:09 +0530

    UPDATE: navbar logics updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import { RiAddFill, RiSearch2Line } from "react-icons/ri";
 const Navbar = ({ user, searchQuery, setSearchQuery }) => {
   const navigate = useNavigate();
 
-  if (!user) return null;
-
   return (
     <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
       <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
@@ -22,9 +20,11 @@ const Navbar = ({ user, searchQuery, setSearchQuery }) => {
         />
       </div>
       <div className="flex gap-3 items-center">
-        <Link to={`user/${user?._id}`} className="hidden md:block">
-          <img src={user?.image} alt="user" className="w-16 rounded-full p-1" />
-        </Link>
+        {user && (
+          <Link to={`user/${user._id}`} className="hidden md:block">
+            <img src={user.image} alt="user" className="w-16 rounded-full p-1" />
+          </Link>
+        )}
         <Link
           to={`post/create`}
           className="bg-accent text-white rounded-lg w-12 h-12 md:w-14 flex justify-center items-center">
