5e734dcfe2dace49de0c714967fa827f1d247467
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-24 21:04:33 +0530

    UPDATE: navbar icons updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { IoMdAdd, IoMdSearch } from "react-icons/io";
+import { RiAddFill, RiSearch2Line } from "react-icons/ri";
 
 const Navbar = ({ user, searchQuery, setSearchQuery }) => {
   const navigate = useNavigate();
@@ -9,7 +9,7 @@ const Navbar = ({ user, searchQuery, setSearchQuery }) => {
   return (
     <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
       <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
-        <IoMdSearch fontSize={20} className="ml-1" />
+        <RiSearch2Line fontSize={22} className="mx-2" />
         <input
           type="text"
           name="search"
@@ -21,14 +21,14 @@ const Navbar = ({ user, searchQuery, setSearchQuery }) => {
           className="p-2 w-full bg-white outline-none"
         />
       </div>
-      <div className="flex gap-3">
+      <div className="flex gap-3 items-center">
         <Link to={`user/${user?._id}`} className="hidden md:block">
-          <img src={user?.image} alt="user" className="w-16 rounded-full" />
+          <img src={user?.image} alt="user" className="w-16 rounded-full p-1" />
         </Link>
         <Link
           to={`post/create`}
-          className="bg-black text-white rounded-lg w-12 h-12 md:w-14 flex justify-center items-center">
-          <IoMdAdd />
+          className="bg-accent text-white rounded-lg w-12 h-12 md:w-14 flex justify-center items-center">
+          <RiAddFill fontSize={20} />
         </Link>
       </div>
     </div>
